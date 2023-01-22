edbb5fc15fc9821af7a9739f160e270a33c10d92
Taisuke Fukuno <fukuno@jig.jp>
2023-01-23 08:31:37 +0900

    add sample2

--- sample2.js
+++ sample2.js
@@ -1,7 +1,10 @@
 import * as esprima from "https://code4fukui.github.io/esprima/es/esprima.min.js";
-import escodegen from "https://code4fukui.github.io/escodegen/escodegen.js";
+//import escodegen from "https://code4fukui.github.io/escodegen/escodegen.js";
+import escodegen from "./escodegen.js";
 
-const program = "const res = await func()";
+const program = "const answer = func();"
+//const tokens = esprima.tokenize(program);
+//console.log(tokens);
 const ast = esprima.parseScript(program);
 console.log(ast);
 const res = escodegen.generate(ast);
